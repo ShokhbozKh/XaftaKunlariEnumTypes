ee7156b9e3f96b196a0f48099bf8921f7578ba47
Shohboz Xurramov <shohboztuit@gmail.com>
2023-09-17 19:15:40 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.SymbolStore;
+
+namespace ConsoleApp2
+{
+    enum LanguageType 
+    {
+        Dushanba=1,
+        Seshanba,
+        Chorshanba,
+        Payshanba,
+        Juma,
+        Shanba,
+        Yakshanba
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Xafta kunini kiriting:");
+                int number = int.Parse(Console.ReadLine());
+                LanguageType language = (LanguageType)number;
+
+                if (number > 0 && number < 6)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"bugun ish kuni:{language}");
+
+                }
+                else if (number == 6 || number == 7)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Dam olish kuni: {language}");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("xatolik bor");
+                }
+
+                
+                Main(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Xatolik bor: {ex.Message}");
+            }
+            
+
+        }
+    }
+}
